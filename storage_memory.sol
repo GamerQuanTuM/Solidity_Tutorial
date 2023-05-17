538973f3a6a5eef2fe2209693bacfd22eed92fe8
Remix Github Token <shuvamsantra10@gmail.com>
2023-05-17 21:56:57 +0530

    storage-memort

---
+++ storage_memory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity >= 0.7.0;
+
+contract MyContract {
+    function print(string memory name) public pure returns(string memory){
+        return name;
+    }
+}
